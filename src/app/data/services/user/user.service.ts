786e78e40aff4e946a77ce548c991be4dd161006
jrgcast <joao.ralph1@gmail.com>
2024-07-01 15:50:34 -0300

    feat: skeleton of application created, WIP

---
+++ src/app/data/services/user/user.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {environment as env} from '../../../../'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly USER_SERVICE_URL = 
+
+  constructor() { }
+}
