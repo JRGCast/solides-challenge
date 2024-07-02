3aa4870e9c8053c3a2e6d1773a53de1bed51c748
jrgcast <joao.ralph1@gmail.com>
2024-07-02 18:48:47 -0300

    refactor: login and create are very similar, wip

--- src/app/pages/landing-page/components/create/create.component.ts
+++ src/app/pages/landing-page/components/create/create.component.ts
@@ -3,11 +3,12 @@ import { CommonHeaderComponent } from '../common-header/common-header.component'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from 'src/app/data/services/user/user.service';
 import { NgTemplateOutlet } from '@angular/common';
+import { CommonButtonComponent } from 'src/app/miscellaneous/common-button/common-button.component';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonHeaderComponent, ReactiveFormsModule, NgTemplateOutlet],
+  imports: [CommonHeaderComponent, CommonButtonComponent, ReactiveFormsModule, NgTemplateOutlet],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
@@ -21,22 +22,52 @@ export class CreateComponent {
     subtitle: 'Enter your information below or continue with social media account'
   }
 
-  passwordInputType: 'text' | 'password' = 'text';
-  passwordInputIcon = this.openEyeIconSrc;
-
   createForm = this.fb.group({
     email: this.fb.control(null, [Validators.required, Validators.email]),
-    phone: this.fb.control(null, [Validators.required, Validators.minLength(9), Validators.pattern("^[0-9]*$")]),
+    mobile: this.fb.control(null, [Validators.required, Validators.minLength(9), Validators.pattern("^[0-9]*$")]),
     password: this.fb.control(null, [Validators.required, Validators.minLength(6)])
   })
 
-  handlePasswordEyeClick(): void {
-    if (this.passwordInputType === 'text') {
-      this.passwordInputType = 'password'
-      this.passwordInputIcon = this.hideEyeIconSrc;
-    } else {
-      this.passwordInputType = 'text'
-      this.passwordInputIcon = this.openEyeIconSrc;
+  readonly inputsConfig = {
+    email: {
+      type: 'text',
+      label: 'Email Address',
+      placeholder: 'Your email address',
+      fControlName: 'email',
+      iconLeftSrc: 'assets/icons/mail.svg'
+    },
+    mobile: {
+      type: 'text',
+      label: 'Mobile Number',
+      placeholder: 'Your mobile number',
+      fControlName: 'mobile',
+      iconLeftSrc: 'assets/icons/mobile.svg',
+    },
+    password: {
+      type: 'password',
+      label: 'Password',
+      placeholder: 'Your password',
+      fControlName: 'password',
+      iconLeftSrc: 'assets/icons/lock.svg',
+      iconRightSrc: 'assets/icons/open-eye.svg'
     }
   }
+
+  readonly commonBtnConfig = {
+    bgColor: '#BC8363',
+    labelTxt: 'Create Account',
+    labelColor: '#FFF'
+  }
+
+  showHidePassword(): void {
+    if (this.inputsConfig.password.type === 'password') {
+      this.inputsConfig.password.type = 'text';
+      this.inputsConfig.password.iconRightSrc = this.hideEyeIconSrc;
+    } else {
+      this.inputsConfig.password.type = 'password';
+      this.inputsConfig.password.iconRightSrc = this.openEyeIconSrc;
+    }
+  }
+
+
 }
--- src/app/pages/landing-page/components/login/login.component.ts
+++ src/app/pages/landing-page/components/login/login.component.ts
@@ -3,17 +3,20 @@ import { CommonHeaderComponent } from '../common-header/common-header.component'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from 'src/app/data/services/user/user.service';
 import { JsonPipe } from '@angular/common';
+import { CommonButtonComponent } from 'src/app/miscellaneous/common-button/common-button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonHeaderComponent, ReactiveFormsModule, JsonPipe],
+  imports: [CommonHeaderComponent, CommonButtonComponent, ReactiveFormsModule, JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent implements OnInit {
   private readonly fb = inject(FormBuilder);
   private readonly userService = inject(UserService);
+  private readonly openEyeIconSrc = '../../../assets/icons/open-eye.svg'
+  private readonly hideEyeIconSrc = '../../../assets/icons/hide-eye.svg'
   readonly headerConfig = {
     title: 'Login',
     subtitle: 'Welcome back! Please login to continue'
@@ -37,9 +40,21 @@ export class LoginComponent implements OnInit {
       label: 'Password',
       placeholder: 'Your password',
       fControlName: 'password',
-      iconLeftSrc: 'assets/icons/mail.svg',
-      iconRightSrc: 'assets/icon/open-eye.svg'
+      iconLeftSrc: 'assets/icons/lock.svg',
+      iconRightSrc: 'assets/icons/open-eye.svg'
     }
+  }
+
+  readonly loginBtnConfig = {
+    bgColor: '#BC8363',
+    labelTxt: 'Login',
+    labelColor: '#FFF'
+  }
+
+  readonly forgotBtnConfig = {
+    bgColor: '#FFF',
+    labelTxt: 'Forgot Password',
+    labelColor: '#BC8363'
   }
 
   ngOnInit(): void {
@@ -47,5 +62,15 @@ export class LoginComponent implements OnInit {
       console.log('changes', c)
       console.log(this.loginForm.get('email'))
     })
+  }
+
+  showHidePassword(): void {
+    if (this.inputsConfig.password.type === 'password') {
+      this.inputsConfig.password.type = 'text';
+      this.inputsConfig.password.iconRightSrc = this.hideEyeIconSrc;
+    } else {
+      this.inputsConfig.password.type = 'password';
+      this.inputsConfig.password.iconRightSrc = this.openEyeIconSrc;
+    }
   }
 }
