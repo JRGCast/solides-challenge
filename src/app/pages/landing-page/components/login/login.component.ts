2cf9bedf855c39801813978df7fc226c6cbdd6bc
jrgcast <joao.ralph1@gmail.com>
2024-07-02 14:59:33 -0300

    delete/refactor: removed unecessary component, refactored usage

--- src/app/pages/landing-page/components/login/login.component.ts
+++ src/app/pages/landing-page/components/login/login.component.ts
@@ -3,12 +3,11 @@ import { CommonHeaderComponent } from '../common-header/common-header.component'
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from 'src/app/data/services/user/user.service';
 import { JsonPipe } from '@angular/common';
-import { ImgIconComponent } from 'src/app/miscellaneous/img-icon/img-icon.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonHeaderComponent, ImgIconComponent, ReactiveFormsModule, JsonPipe],
+  imports: [CommonHeaderComponent, ReactiveFormsModule, JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -25,12 +24,23 @@ export class LoginComponent implements OnInit {
     password: this.fb.control(null, { updateOn: 'blur', validators: [Validators.required, Validators.minLength(6)] })
   })
 
-  readonly inputsConfigs = [{
-    type: 'text',
-    label: 'Test',
-    placeholder: '',
-    fControlName: 'email',
-  }]
+  readonly inputsConfig = {
+    email: {
+      type: 'text',
+      label: 'Email Address',
+      placeholder: 'Your email address',
+      fControlName: 'email',
+      iconLeftSrc: 'assets/icons/mail.svg'
+    },
+    password: {
+      type: 'password',
+      label: 'Password',
+      placeholder: 'Your password',
+      fControlName: 'password',
+      iconLeftSrc: 'assets/icons/mail.svg',
+      iconRightSrc: 'assets/icon/open-eye.svg'
+    }
+  }
 
   ngOnInit(): void {
     this.loginForm.valueChanges.subscribe(c => {
