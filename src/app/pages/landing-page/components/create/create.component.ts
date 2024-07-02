439a0c7bb14539d675c5a5fc357bdb4061d3f942
jrgcast <joao.ralph1@gmail.com>
2024-07-02 04:06:18 -0300

    feat: create and login component in the middle of ocmpletion

--- src/app/pages/landing-page/components/create/create.component.ts
+++ src/app/pages/landing-page/components/create/create.component.ts
@@ -1,16 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonHeaderComponent } from '../common-header/common-header.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from 'src/app/data/services/user/user.service';
+import { NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CommonHeaderComponent],
+  imports: [CommonHeaderComponent, ReactiveFormsModule, NgTemplateOutlet],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  private readonly fb = inject(FormBuilder);
+  private readonly userService = inject(UserService);
+  private readonly openEyeIconSrc = '../../../assets/icons/open-eye.svg'
+  private readonly hideEyeIconSrc = '../../../assets/icons/hide-eye.svg'
   readonly headerConfig = {
     title: 'Create Account',
     subtitle: 'Enter your information below or continue with social media account'
+  }
+
+  passwordInputType: 'text' | 'password' = 'text';
+  passwordInputIcon = this.openEyeIconSrc;
+
+  createForm = this.fb.group({
+    email: this.fb.control(null, [Validators.required, Validators.email]),
+    phone: this.fb.control(null, [Validators.required, Validators.minLength(9), Validators.pattern("^[0-9]*$")]),
+    password: this.fb.control(null, [Validators.required, Validators.minLength(6)])
+  })
+
+  handlePasswordEyeClick(): void {
+    if (this.passwordInputType === 'text') {
+      this.passwordInputType = 'password'
+      this.passwordInputIcon = this.hideEyeIconSrc;
+    } else {
+      this.passwordInputType = 'text'
+      this.passwordInputIcon = this.openEyeIconSrc;
+    }
   }
 }
--- src/app/pages/landing-page/components/login/login.component.ts
+++ src/app/pages/landing-page/components/login/login.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonHeaderComponent } from '../common-header/common-header.component';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from 'src/app/data/services/user/user.service';
+import { JsonPipe } from '@angular/common';
+import { ImgIconComponent } from 'src/app/miscellaneous/img-icon/img-icon.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonHeaderComponent],
+  imports: [CommonHeaderComponent, ImgIconComponent, ReactiveFormsModule, JsonPipe],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private readonly fb = inject(FormBuilder);
+  private readonly userService = inject(UserService);
   readonly headerConfig = {
     title: 'Login',
     subtitle: 'Welcome back! Please login to continue'
+  }
+
+  loginForm = this.fb.group({
+    email: this.fb.control(null, { updateOn: 'blur', validators: [Validators.required, Validators.email] }),
+    password: this.fb.control(null, { updateOn: 'blur', validators: [Validators.required, Validators.minLength(6)] })
+  })
+
+  readonly inputsConfigs = [{
+    type: 'text',
+    label: 'Test',
+    placeholder: '',
+    fControlName: 'email',
+  }]
+
+  ngOnInit(): void {
+    this.loginForm.valueChanges.subscribe(c => {
+      console.log('changes', c)
+      console.log(this.loginForm.get('email'))
+    })
   }
 }
